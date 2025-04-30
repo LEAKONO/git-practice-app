4bb112da1b1004cf11b5acb4899514b34e46639f
LEAKONO <leakonoemmanuel3@gmail.com>
2025-04-30 17:21:59 +0300

    Create basic homepage component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import Header from './components/Header';
 import GreetingButton from './components/GreetingButton';
+import Home from './components/Home';
+
 
 function App() {
   return (
     <div>
       <Header />
+      <Home />
       <GreetingButton />
     </div>
   );
---
+++ src/components/Home.jsx
@@ -0,0 +1,8 @@
+export default function Home() {
+    return (
+      <div className="min-h-screen bg-gray-100 p-8">
+        <h1 className="text-3xl font-bold text-blue-600">Welcome to Git Practice!</h1>
+        <p className="mt-4 text-gray-700">This project helps you learn Git commands.</p>
+      </div>
+    );
+  }
